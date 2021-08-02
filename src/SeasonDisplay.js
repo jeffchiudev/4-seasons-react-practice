36f210c1fb643229262999004b79113b6d93fd8e
Jeff Chiu <jwchiu82@gmail.com>
2021-08-02 16:51:14 -0700

    add functionality for all seasons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -6,20 +6,35 @@ const seasonConfig = {
     text: "Let's hit the beach!",
     iconName: "sun",
   },
+  fall: {
+    text: "Leaves are starting to change!",
+    iconName: "leaf",
+  },
   winter: {
     text: "Burr it's cold!",
     iconName: "snowflake",
   },
+  spring: {
+    text: "Lets go smell the flowers!",
+    iconName: "tree",
+  },
 };
 
 const getSeason = (lat, month) => {
-  if (month > 2 && month < 9) {
+  if (month > 4 && month < 8) {
     return lat > 0 ? "summer" : "winter";
-  } else {
-    return lat < 0 ? "winter" : "summer";
+  } else if (month > 7 && month < 11) {
+    return lat > 0 ? "fall" : "spring";
+  } else if (month > 10 || month < 2) {
+    return lat > 0 ? "winter" : "summer";
+  } else if (month > 1 && month < 5) {
+    return lat > 0 ? "spring" : "fall;";
   }
 };
-
+//11 to 1 = dec to feb, winter
+//2 to 4 = mar to may, spring
+//5 to 7 = june to aug, summer
+//8 to 10 = sep to nov, fall
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   const { text, iconName } = seasonConfig[season];
